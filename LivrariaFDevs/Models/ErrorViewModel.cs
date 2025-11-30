95becde4df8427d5cef692ac2cbbd0f6ff613f92
giovana-cruzado <giovanarusbe@gmail.com>
2025-11-30 10:07:07 -0300

    Criação do projeto

---
+++ LivrariaFDevs/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LivrariaFDevs.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
